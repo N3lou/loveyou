912afc9d02ff50ccdd443d1ddb94d85e9e0e8ba6
Nelo <itztigerblox@gmail.com>
2025-06-16 22:33:59 +0800

    sigma changes

---
+++ hello.py
@@ -0,0 +1,8 @@
+import random
+
+random_number = random.randint(0,4)
+
+sigmas=["ewan", "bembem", "bonbon", "alpha", "what"]
+
+print(f"The Previous Letter Was:  {str(random_number)}")
+print(f"yes:  {sigmas[random_number]}")
