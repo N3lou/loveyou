d743ec10f6b7eb6a8ca8a12c21ebd48c60e440d6
N3lou <ligrue7@gmail.com>
2025-06-17 16:09:44 +0800

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-import random
-
-random_number = random.randint(0,4)
-
-sigmas=["ewan", "bembem", "bonbon", "alpha", "what"]
-
-print(f"The Previous Letter Was:  {str(random_number)}")
-print(f"yes:  {sigmas[random_number]}")
